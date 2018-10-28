3ab141758cb2885c2c1b9e4399fbffb6420916ab
Jessé Oliveira <jesse.oliveira@outlook.com>
2018-10-28 14:59:36 -0300

    Adicioniei array carros

--- array_multidimensionais.php
+++ array_multidimensionais.php
@@ -1,4 +1,14 @@
 <?php 
 
+$carros = array();
+
+$carros['Palio']['cor'] = 'Azul';
+$carros['Palio']['potência'] = '1.0';
+$carros['Palio']['opcionais'] = 'Ar Cond.';
+$carros['Corsa']['cor'] = 'Vermelho';
+$carros['Corsa']['potência'] = '1.2';
+$carros['Corsa']['opcionais'] = 'mp3';
+
+
 
  ?>
