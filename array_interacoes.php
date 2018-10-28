704760e6d4c423d8d3263c0bba0f51a85aadb49c
Jessé Oliveira <jesse.oliveira@outlook.com>
2018-10-28 14:53:34 -0300

    Criação do arquivo array_interacoes.php

---
+++ array_interacoes.php
@@ -0,0 +1,13 @@
+<?php 
+
+$fruta = array();
+
+$fruta['cor'] = 'vermelho';
+$fruta['sabor'] = 'doce';
+$fruta['nome'] = 'maça';
+
+foreach ($fruta as $chave => $frutas) {
+	echo "$chave : $frutas <br>\n";
+}
+
+ ?>
