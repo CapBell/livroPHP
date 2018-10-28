838e4bff7f2303edd45234d3af71b24d90f15962
Jessé Oliveira <jesse.oliveira@outlook.com>
2018-10-28 15:03:59 -0300

    foi adicionado foreach para exibição dos  carros

--- array_multidimensionais.php
+++ array_multidimensionais.php
@@ -9,6 +9,11 @@ $carros['Corsa']['cor'] = 'Vermelho';
 $carros['Corsa']['potência'] = '1.2';
 $carros['Corsa']['opcionais'] = 'mp3';
 
-
+foreach ($carros as $modelo => $caracteristicas){
+	echo "=> modelo $modelo<br>\n";
+	foreach ($caracteristicas as $caracteristicas => $valor) {
+		echo " - características $caracteristicas => $valor <br>\n";
+	}
+}
 
  ?>
