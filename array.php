07ce1c7eec250de3d568d1553d911a679c355a44
Jessé Oliveira <jesse.oliveira@outlook.com>
2018-10-28 14:40:17 -0300

    Criando arrays

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php 
+	$cores = array(0=> 'vermelho', 1=> 'Preto', 2=> 'Amarelo');
+
+	$cores = array('vermelho', 'preto', 'amarelo');
+ 
+	$cores = ['vermelho', 'preto', 'amarelo'];
+
+	$nomes[]= 'maria';
+	$nomes[]= 'josé';
+	$nomes[]= 'sara';
+	$nomes[]= 'vanessa';
+
+	echo $cores[0].PHP_EOL;
+	echo $cores[1].PHP_EOL;
+	echo $cores[2].PHP_EOL;
+ ?>
