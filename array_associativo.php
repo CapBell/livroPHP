fdf59023354d555a1fc9e8aab1e72e01c6425981
Jessé Oliveira <jesse.oliveira@outlook.com>
2018-10-28 14:47:23 -0300

    a chave pod ser string ou integer não negativo, o valor pode ser qualquer tipo

--- array_associativo.php
+++ array_associativo.php
@@ -1,3 +1,19 @@
 <?php 
 
+$pessoa = array();
+
+$pessoa['nome'] = 'Maria da silva';
+$pessoa['rua'] = 'Sao João';
+$pessoa['bairro'] = 'Cidade Alta';
+$pessoa['cidade'] = 'Porto Alegre';
+
+echo $pessoa['nome'];
+echo "<br>";
+echo $pessoa['rua'];
+echo "<br>";
+echo $pessoa['bairro'];
+echo "<br>";
+echo $pessoa['cidade'];
+echo "<br>";
+
  ?>
